c58d4ff1fde58464197c8a0458e6373793f2c215
Maharavo1 <hei.maharavo@gmail.com>
2024-02-05 10:08:33 +0300

    feat : add accueil.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import Link from "next/link"
 import Head from "next/head";
 import Login from "./login";
+import Accueil from "./accueil/accueil";
 export default function Home() {
   return (
     <div>
     <Head>
       <title>Google Docs</title>
     </Head>
-    <Login/>
+    <Accueil/>
     </div>
   )
 }
